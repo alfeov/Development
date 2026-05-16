57b15b7a97caaa82991add5fd13fbb089c7a61be
alfeov <alfeove@gmail.com>
2026-05-16 10:53:54 +0300

    Pet-Project init, CodeWars Class Task

--- CodeWars/The Greatest Warrior/app.js
+++ CodeWars/The Greatest Warrior/app.js
@@ -22,10 +22,38 @@ class Warrior {
     return this.#achievements
   }
 
-  battle(enemyLevel) {}
+  battle(enemyLevel) {
+    if (!(enemyLevel >= 1 && enemyLevel <= 100)) {
+      return 'Invalid level'
+    }
+    const diff = enemyLevel - this.#level
+    let response = 'Easy fight'
+    if (diff > 0) {
+      if (
+        Math.floor(this.#level / 10) !== Math.floor(enemyLevel / 10) &&
+        diff >= 5
+      )
+        return "You've been defeated"
+      response = 'An intense fight'
+      this.#increaseExperience(20 * diff * diff)
+    } else if (diff === 0) {
+      response = 'A good fight'
+      this.#increaseExperience(10)
+    } else if (diff === -1) {
+      response = 'A good fight'
+      this.#increaseExperience(5)
+    }
+
+    return response
+  }
   training([achievementDescription, experienceGain, levelReq]) {
     if (this.#level < levelReq) return 'Not strong enough'
     this.#achievements = [...this.#achievements, achievementDescription]
+    this.#increaseExperience(experienceGain)
+    return achievementDescription
+  }
+
+  #increaseExperience(experienceGain) {
     this.#experience = Math.min(this.#experience + experienceGain, 10000)
     this.#changeRank()
     this.#changeLevel()
@@ -49,6 +77,6 @@ class Warrior {
   }
 
   #changeLevel() {
-    this.#level = this.#experience / 100
+    this.#level = Math.floor(this.#experience / 100)
   }
 }
---
+++ Pet-Projects/To Do List/plan.md
@@ -0,0 +1,22 @@
+## Plan
+
+- [x] Настроить начальную сборку приложения
+- [x] Скачать Assets
+- [x] Подключить шрифты
+
+## UI
+
+- Header
+- New Task Section
+- Search Task Section
+- Counter
+- Delete
+- Task List
+- Tasks
+  - Checkbox
+  - Text
+  - Close
+
+## Logic
+
+- [ ]
