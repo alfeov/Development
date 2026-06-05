360b90475ecc2f4518b64bbafc385a4bf4aa98ef
alfeov <alfeove@gmail.com>
2026-06-05 16:05:40 +0300

    Fortnite Shop project init

--- MishaNep Course/React/React Timer/src/Timer.jsx
+++ MishaNep Course/React/React Timer/src/Timer.jsx
@@ -1,43 +1,64 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useReducer } from 'react'
 
 const localStorageKey = 'count'
 
+function createInitialState() {
+  return {
+    count: JSON.parse(localStorage.getItem(localStorageKey)) ?? 0,
+    isCounting: false,
+  }
+}
+
+function countReducer(state, { type, num = 1 }) {
+  if (type === 'START') {
+    return {
+      ...state,
+      isCounting: true,
+    }
+  }
+  if (type === 'STOP') {
+    return {
+      ...state,
+      isCounting: false,
+    }
+  }
+  if (type === 'RESET') {
+    return {
+      count: 0,
+      isCounting: false,
+    }
+  }
+  if (type === 'COUNT') {
+    return {
+      ...state,
+      count: state.count + num,
+    }
+  }
+  return state
+}
+
 export default function Timer() {
-  const [count, setCount] = useState(
-    () => JSON.parse(localStorage.getItem(localStorageKey)) ?? 0,
+  const [{ count, isCounting }, dispatch] = useReducer(
+    countReducer,
+    null,
+    createInitialState,
   )
-  const [isCounting, setIsCounting] = useState(false)
-
-  const intervalIdRef = useRef(null)
-
-  const handleStart = () => {
-    setIsCounting(true)
-    intervalIdRef.current = setInterval(() => {
-      setCount((prevCount) => prevCount + 1)
-    }, 1000)
-  }
-
-  const handleStop = () => {
-    clearInterval(intervalIdRef.current)
-    setIsCounting(false)
-  }
-
-  const handleReset = () => {
-    if (isCounting) handleStop()
-    setCount(0)
-  }
 
   useEffect(() => {
     localStorage.setItem(localStorageKey, JSON.stringify(count))
   }, [count])
 
   useEffect(() => {
-    return () => {
-      if (intervalIdRef.current) {
-        clearInterval(intervalIdRef.current)
-      }
+    let intervalId = null
+    if (isCounting) {
+      intervalId = setInterval(() => {
+        dispatch({ type: 'COUNT' })
+      }, 1000)
     }
-  }, [])
+    return () => {
+      if (intervalId) clearInterval(intervalId)
+    }
+  }, [isCounting])
 
   return (
     <div className='App'>
@@ -45,11 +66,11 @@ export default function Timer() {
       <h3>{count}</h3>
       <div>
         {isCounting ? (
-          <button onClick={handleStop}>Stop</button>
+          <button onClick={() => dispatch({ type: 'STOP' })}>Stop</button>
         ) : (
-          <button onClick={handleStart}>Start</button>
+          <button onClick={() => dispatch({ type: 'START' })}>Start</button>
         )}
-        <button onClick={handleReset}>Reset</button>
+        <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
       </div>
     </div>
   )
